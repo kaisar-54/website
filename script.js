8d9ef8816aa9dd7c675779e1bd5b1d27ae053902
kaisar-54 <ignitesnow121@gmail.com>
2025-08-13 21:24:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 document.querySelectorAll(".project-item img").forEach(img => {
   img.addEventListener("mouseenter", () => {
     img.style.boxShadow = "0 0 15px rgba(0, 178, 248, 0.8)";
@@ -7,3 +8,12 @@ document.querySelectorAll(".project-item img").forEach(img => {
   });
 });
 
+
+document.querySelectorAll(".about-image img").forEach(img => {
+  img.addEventListener("mouseenter", () => {
+    img.style.boxShadow = "0 0 15px rgba(0, 178, 248, 0.8)";
+  });
+  img.addEventListener("mouseleave", () => {
+    img.style.boxShadow = "0 2px 10px rgba(0,0,0,0.1)";
+  });
+});
